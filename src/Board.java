011a1833031f12b4198dbf9d08353411fe93046d
burnttoasty2k <kjpadget@ncsu.edu>
2025-04-09 16:45:21 -0400

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,21 @@
 import java.util.*;
 
 public class Board {
-    public int[] ROWS;
-    public int[] COLUMNS;
-    public int[][] GAME_BOARD;
+    private int[] ROWS;
+    private int[] COLUMNS;
+    
+    private char playerOneElement = 'X';
+    private char playerTwoElement = 'O';
+    
+    public char currentElement;
+    public int elementCount = 0;
+    
+    public Board {
+        private int[][] GAME_BOARD = new int[ROWS][COLUMNS];
+    }
     
     
+       
     
     
     
