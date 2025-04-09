e89b434c4a543e769ce134cfe3c24222842f3f85
xhong06 <xander.hong06@gmail.com>
2025-04-09 17:09:04 -0400

    4/9 Updated Game and Player Files
    These are the essentially complete Game and Player Java Files.

---
+++ src/Game.java
@@ -0,0 +1,106 @@
+import java.util.*;
+
+
+
+public class Game {
+	
+	private Player playerOne;
+	private Player playerTwo;
+	private int playerTurn;
+	private int column; 
+	
+	public Game(Player playerOne, Player playerTwo) {
+		
+		this.playerOne = playerOne;
+		this.playerTwo = playerTwo;
+		playerTurn = 0;
+		
+	}
+	
+	public Player getPlayerOne() {
+		return playerOne;
+	}
+	
+	public Player getPlayerTwo() {
+		return playerTwo;
+	}
+	
+	public int getPlayerTurn() {
+		return playerTurn;
+	}
+	
+	public int getColumn() {
+		return column; 
+	}
+	
+	public void getPlayerInput(int columnInput) {
+		getColumn = columnInput;
+	}
+	
+	public void playerOneWin() {
+		playerOne.addWin();
+		System.out.println("Congratulations " + playerOne.getName() 
+		+ " for winning the game!");
+		System.out.print("Want to play again? (y/n): ");
+	}
+	
+	public void playerTwoWin() {
+		playerTwo.addWin();
+		System.out.println("Congratulations " + playerTwo.getName() 
+		+ " for winning the game!");
+		System.out.print("Want to play again? (y/n): ");
+	}
+	
+}
+
+	// 3|x|_|_|_|_|_|_|_|
+	// 2|o|o|_|_|_|_|_|_|
+	// 1|x|_|o|_|_|_|_|_|
+	// 0|o|_|_|o|_|_|_|_|
+
+// for(int i = ROWS; i >= 7; i++) {
+	// if(!(Game_Board[i][playersChoice] == 'x' || 'o')){
+		// Game_Board[i][playersChoice] = playerChar;
+		// break;
+	// }
+	
+// }
+
+
+
+
+// int columnInput = scnr.nextInt();
+	
+	// while(scnr.hasNext()){
+		// if(scnr.hasNextInt()){
+			//getColumn = scnr.nextInt();
+		// } else {
+			// scnr.next();
+			// System.out.println("Please input a column number 1-8.");
+		// }
+	// }
+	
+	
+	// for(int i = 0; i < ROWS; i++) {
+		// for(int j = 0; j < COLUMNS; j++) {
+			// System.out.print("|" + GAME_BOARD[i][j]);
+			
+		// }
+		// System.out.println("|");
+	// }
+	
+	// for(int i = 0; i < MORE_ROWS; i++) {
+		// for(int j = 0; j < MORE_COLUMNS; j++) {
+			// System.out.print("|" + GAME_BOARD[i][j]);
+			
+		// }
+		// System.out.println("|");
+	// }
+	
+	
+	
+	// if(playerTurn == 0){
+		// getRightDiagonal(playerOneElement);
+	// }else{
+		// getRightDiagonal(playerOneElement);
+	// }
--- src/Player.java
+++ src/Player.java
@@ -7,22 +7,27 @@ public class Player {
 	private String name;
 	private int wins;
 	
-	
 	public Player(String name) {
 		this.name = name;
 		wins = 0;
-		
-		
-		
 	}
 	
 	public Player(String name, int wins) {
 		this.name = name;
 		this.wins = wins;
-		
-		
 	}
 	
+	public String getName() {
+		return name;
+	}
+	
+	public int getWins() {
+		return wins;
+	}
+	
+	public void addWin() {
+		wins++;
+	}
 	
 	
 }
