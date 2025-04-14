d0d670ffc6c58afdbd127c21922f59a9da9e1dfe
xhong06 <xander.hong06@gmail.com>
2025-04-14 15:19:06 -0400

    Deleted ROWS and COLUMNS constant arrays.
    Deleted Constant Arrays ROWS & COLUMNS since they were not necessary.

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,7 @@
 import java.util.*;
 
 public class Board {
-    public int[] ROWS = {8, 12};
-    public int[] COLUMNS = {8, 12};
+	
     public char[][] GAME_BOARD;
 	
 	private char playerOneElement = 'x';
@@ -37,7 +36,7 @@ public class Board {
 	}
     
     void addElement(int playersChoice, char playerChar) {
-		for(int i = ROWS; i >= 7; i++) {
+		for(int i = rows; i >= 7; i++) {
 			if(!(Game_Board[i][playersChoice] == 'x' || 'o')){
 				Game_Board[i][playersChoice] = playerChar;
 				break;
